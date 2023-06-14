c9e90911cfb7b7b2af90c3a15ffee0eb3aef492d
Asped <drukon2012@ya.ru>
2023-06-15 00:11:11 +0300

    dz

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+==============================================================================================
+Console.Write("Напишите 1 число");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.Write("Напишите 2 число");
+int numb2 = int.Parse(Console.ReadLine()!);
+
+if (numb1 < numb2)
+{
+        Console.WriteLine(numb2 + " больше " + numb1);
+}
+else{
+     Console.WriteLine(numb1 + " больше " + numb2);
+}*/
+//==============================================================================================
+/*
+ Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+//======================================================================================
+/*
+Console.Write("Напишите 1 число");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.Write("Напишите 2 число");
+int numb2 = int.Parse(Console.ReadLine()!);
+Console.Write("Напишите 3 число");
+int numb3 = int.Parse(Console.ReadLine()!);
+
+
+if (numb1 < numb2)
+{
+    if (numb2 > numb3)
+    {
+        Console.WriteLine(numb2 + ("Максимальное число"));
+    }
+    else{
+         Console.WriteLine(numb3 + ("Максимальное число"));
+    }
+}
+else{
+  if (numb1 > numb3)
+  {
+    Console.WriteLine(numb1 + (" Максимальное число"));
+  }
+  else{
+     Console.WriteLine(numb3 + (" Максимальное число"));
+  }
+}
+*/
