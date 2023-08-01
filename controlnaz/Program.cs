7547576dcfeb020eca36b8ad90a653c11f8d5ae5
Asped <drukon2012@ya.ru>
2023-08-02 00:50:26 +0300

    создание второго массива

--- controlnaz/Program.cs
+++ controlnaz/Program.cs
@@ -1,3 +1,16 @@
 ﻿string [] bart  = new String [5] { "helow23", "2", "hi", "=)", "Element 5" };
 
+string [] print ( string [] a){
+
+string [] pr = new string [a.GetLength(0)];
+for (int i = 0; i < pr.GetLength(0); i++)
+{
+    if (a[i].Length <= 3)
+    {
+        pr [i] = a[i];
+    }
+    
+}
+return pr;
+}
 
